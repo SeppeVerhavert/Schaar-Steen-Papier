1f77682e888522e51f03ac6cb0acf7017f667134
seppe_verhavert <seppe.verhavert@gmail.com>
2019-06-17 10:56:21 +0200

    add variables rock, paper, scissors

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 document.getElementById("rockBtn").addEventListener("click", showRock);
 document.getElementById("paperBtn").addEventListener("click", showPaper);
 document.getElementById("scissorsBtn").addEventListener("click", showScissors);
-
 document.getElementById("ComputerBtn").addEventListener("click", showRandom);
+document.getElementById("resetBtn").addEventListener("click", resetFields);
+
+var rock = "<i class=\"fas fa-hand-rock fa-4x\"></i>";
+var paper = "<i class=\"fas fa-hand-paper fa-4x\"></i>";
+var scissors = "<i class=\"fas fa-hand-scissors fa-4x\">";
 
 function showRock() {
-    document.getElementById("iconArea").innerHTML = "<i class=\"fas fa-hand-rock fa-4x\"></i>";
+    document.getElementById("iconArea").innerHTML = rock;
+    document.getElementById("computerIconArea").innerHTML = " ";
 }
 function showPaper() {
-    document.getElementById("iconArea").innerHTML = "<i class=\"fas fa-hand-paper fa-4x\"></i>";
+    document.getElementById("iconArea").innerHTML = paper;
+    document.getElementById("computerIconArea").innerHTML = " ";
 }
 function showScissors() {
-    document.getElementById("iconArea").innerHTML = "<i class=\"fas fa-hand-scissors fa-4x\">";
+    document.getElementById("iconArea").innerHTML = scissors;
+    document.getElementById("computerIconArea").innerHTML = " ";
 }
 
 function showRandom() {
@@ -25,3 +32,15 @@ function showRandom() {
 
     document.getElementById("computerIconArea").innerHTML = rand;
 }
+
+function resetFields() {
+    console.log("yay");
+    document.getElementById("iconArea").innerHTML = " ";
+    document.getElementById("computerIconArea").innerHTML = " ";
+}
+
+// function compareFields() {
+//     if (rand === ){
+
+//     }
+// }
