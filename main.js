bf234c7a830c6238a35adb05ed156e83f60648e4
seppe_verhavert <seppe.verhavert@gmail.com>
2019-06-17 10:16:45 +0200

    add player box & computer box

--- main.js
+++ main.js
@@ -2,12 +2,18 @@ document.getElementById("rockBtn").addEventListener("click", showRock);
 document.getElementById("paperBtn").addEventListener("click", showPaper);
 document.getElementById("scissorsBtn").addEventListener("click", showScissors);
 
+document.getElementById("ComputerBtn").addEventListener("click", showRandom);
+
 function showRock() {
-    document.getElementById("iconArea").innerHTML = "<i class=\"fas fa-hand-rock fa-2x\"></i>";
+    document.getElementById("iconArea").innerHTML = "<i class=\"fas fa-hand-rock fa-4x\"></i>";
 }
 function showPaper() {
-    document.getElementById("iconArea").innerHTML = "<i class=\"fas fa-hand-paper fa-2x\"></i>";
+    document.getElementById("iconArea").innerHTML = "<i class=\"fas fa-hand-paper fa-4x\"></i>";
 }
 function showScissors() {
-    document.getElementById("iconArea").innerHTML = "<i class=\"fas fa-hand-scissors fa-2x\">";
+    document.getElementById("iconArea").innerHTML = "<i class=\"fas fa-hand-scissors fa-4x\">";
+}
+
+function showRandom() {
+    document.getElementById("computerIconArea").innerHTML = "<i class=\"fas fa-hand-scissors fa-4x\">";
 }
